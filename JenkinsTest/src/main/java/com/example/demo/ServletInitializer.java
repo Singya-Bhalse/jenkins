b18804cc83450dcff57c65444e3be76f490280eb
sagar <singya.bhalse@computronics.in>
2019-02-25 13:11:18 +0530

    new clone

--- JenkinsTest/src/main/java/com/example/demo/ServletInitializer.java
+++ JenkinsTest/src/main/java/com/example/demo/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(JenkinTestApplication.class);
+		return application.sources(JenkinTest1Application.class);
 	}
 
 }
